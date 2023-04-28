f1ede2c6032e072d389bc34c04602349676835d2
nalantianyi <103520146@qq.com>
2023-04-28 14:23:00 +0800

    冒泡排序

--- src/sort/bubble.ts
+++ src/sort/bubble.ts
@@ -6,57 +6,44 @@ import {swap} from '../until';
 
 //基础版本
 function bubbleSort1(array: Array<any>) {
-    const newArray=[...array];
     let n = array.length;
-    let m = 0;
     for (let i = 1; i < n; i++) {
         for (let j = 0; j < n - i; j++) {
-            if (newArray[j] > newArray[j + 1]) {
-                swap(newArray, j, j + 1);
-                m++;
+            if (array[j] > array[j + 1]) {
+                swap(array, j, j + 1);
             }
         }
     }
-    console.log(`交换${m}次`);
-    return newArray;
 }
 
 
 //优化方案1
 function bubbleSort2(array: Array<any>) {
-    const newArray=[...array];
     let n = array.length;
-    let m = 0;
     for (let i = 1; i < n; i++) {
         let hasSort = true;
         for (let j = 0; j < n - i; j++) {
-            if (newArray[j] > newArray[j + 1]) {
-                swap(newArray, j, j + 1);
+            if (array[j] > array[j + 1]) {
+                swap(array, j, j + 1);
                 hasSort = false;
-                m++;
             }
         }
         if (hasSort) {
             break;
         }
     }
-    console.log(`交换${m}次`);
-    return newArray;
 }
 
 //优化方案2，大数下沉的策略
 function bubbleSort3(array: Array<any>) {
-    const newArray=[...array];
     let n = array.length, k = n - 1, swapPos = 0;
-    let m = 0;
     for (let i = 1; i < n; i++) {
         let hasSort = true;
         for (let j = 0; j < k; j++) {
-            if (newArray[j] < newArray[j + 1]) {
-                swap(newArray, j, j + 1);
+            if (array[j] > array[j + 1]) {
+                swap(array, j, j + 1);
                 hasSort = false;
                 swapPos = k;
-                m++;
             }
         }
         if (hasSort) {
@@ -64,25 +51,42 @@ function bubbleSort3(array: Array<any>) {
         }
         k = swapPos;
     }
-    console.log(`交换${m}次`);
-    return newArray;
 }
 
 
-const arr1 = [3, 6, 4, 2, 11, 10, 5];
-console.log('数组1', arr1);
-console.log('方法1：');
-bubbleSort1(arr1);
-console.log('方法2：');
-bubbleSort2(arr1)
-console.log('方法3：');
-bubbleSort3(arr1)
+// 类似于数组的洗牌方法，把生成的数据进行打乱
+function shuffle(a: Array<any>) {
+    let len = a.length;
+    for (let i = 0; i < len - 1; i++) {
+        let index = parseInt(String(Math.random() * (len - i)));
+        let temp = a[index];
+        a[index] = a[len - i - 1];
+        a[len - i - 1] = temp;
+    }
+}
 
-const arr2 = [1, 2, 3, 4, 5, 6];
-console.log('数组2', arr2);
-console.log('方法1：');
-bubbleSort1(arr2);
-console.log('方法2：');
-bubbleSort2(arr2)
-console.log('方法3：');
-bubbleSort3(arr2)
+function test(sortFn: Function) {
+    let array = [];
+    for (let i = 0; i < 10000; i++) {
+        if (i <= 1000) {
+            array[i] = 1000 - i;
+        } else {
+            array[i] = i;
+        }
+    }
+    console.log('========');
+    let start = Date.now();
+    sortFn(array);
+    console.log('部分有序的情况', sortFn.name, Date.now() - start);
+    shuffle(array);
+    start = Date.now();
+    sortFn(array);
+    console.log('完全乱序的情况', sortFn.name, Date.now() - start);
+}
+
+
+test(bubbleSort1);
+test(bubbleSort2);
+test(bubbleSort3);
+
+
